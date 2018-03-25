9a7749dad29d5fef86d40b81e534968b814d0be1
Petter Wallin <pewa@wrk-win-pewa.easit.lan>
2018-03-25 19:43:24 +0200

    Settings.h

---
+++ Settings.h
@@ -0,0 +1,17 @@
+/*
+ * Settings.h
+ *
+ *  Created on: 25 mars 2018
+ *      Author: pewa
+ */
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+#define SSID "ssid"
+#define WIFI_PASSWORD "pwd"
+#define MQTT_SERVER "server.com"
+#define MQTT_CLIENT_NAME "client"
+#define MQTT_USERNAME "user"
+#define MQTT_PASSWORD "password"
+#endif /* SETTINGS_H_ */
