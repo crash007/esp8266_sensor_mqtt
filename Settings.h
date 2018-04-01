cd94bf1630bbd32ffeb060907dd86de0fcb54712
Petter Wallin <pewa@wrk-win-pewa.easit.lan>
2018-04-01 21:29:01 +0200

    ds18b20 address in settings.h

--- Settings.h
+++ Settings.h
@@ -14,4 +14,5 @@
 #define MQTT_CLIENT_NAME "client"
 #define MQTT_USERNAME "user"
 #define MQTT_PASSWORD "password"
+#define DS18B20_ADDRESS { 0x28, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
 #endif /* SETTINGS_H_ */
